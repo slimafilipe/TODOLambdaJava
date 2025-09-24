ea5a68e0fabf71c7f45c1d06ec9f4ab4406c98f2
slimafilipe <limafilipe.coding@gmail.com>
2025-09-24 00:31:09 -0300

    feat: add POJO Task.java

---
+++ src/main/java/dev/filipe/TODOLambdaJava/Model/Task.java
@@ -0,0 +1,78 @@
+package dev.filipe.TODOLambdaJava.Model;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@DynamoDbBean
+public class Task {
+    private String userId;
+    private String taskId;
+    private String title;
+    private String description;
+    private boolean completed;
+    private String CreatedAt;
+
+
+    public Task() {
+    }
+
+
+    public Task(String userId, String taskId, String title, String description, boolean completed, String createdAt) {
+        this.userId = userId;
+        this.taskId = taskId;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+        CreatedAt = createdAt;
+    }
+
+    @DynamoDbPartitionKey
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String id) {
+        this.userId = userId;
+    }
+
+    @DynamoDbPartitionKey
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String id) {
+        this.taskId = taskId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getCreatedAt() {
+        return CreatedAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        CreatedAt = createdAt;
+    }
+}
