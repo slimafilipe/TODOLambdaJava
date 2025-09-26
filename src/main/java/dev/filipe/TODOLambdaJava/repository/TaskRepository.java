63d2ec31be79be83bcaf1ce5da91bc78e63ed163
slimafilipe <limafilipe.coding@gmail.com>
2025-09-26 16:09:20 -0300

    feat: add test unit LinkTaskHandler.java

--- src/main/java/dev/filipe/TODOLambdaJava/repository/TaskRepository.java
+++ src/main/java/dev/filipe/TODOLambdaJava/repository/TaskRepository.java
@@ -1,22 +1,27 @@
 package dev.filipe.TODOLambdaJava.repository;
 
+import com.google.gson.Gson;
 import dev.filipe.TODOLambdaJava.Model.Task;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TaskRepository {
-    private static DynamoDbTable<Task> taskTable;;
-    // Construtor para injeção de dependência em testes
+    private final DynamoDbTable<Task> taskTable;;
+
     public TaskRepository(DynamoDbTable<Task> taskTable) {
-        TaskRepository.taskTable = taskTable;
+        this.taskTable = taskTable;
     }
 
-    public static List<Task> listTasks(String userId) {
+    public  List<Task> listTasks(String userId) {
         QueryConditional conditional =  QueryConditional.keyEqualTo(Key.builder().partitionValue(userId).build());
 
-        return taskTable.query(conditional).items().stream().toList();
+        return taskTable.query(conditional).items().stream().collect(Collectors.toList());
     }
 }
---
+++ src/test/java/dev/filipe/TODOLambdaJava/controller/ListTaskHandlerTest.java
@@ -0,0 +1,77 @@
+package dev.filipe.TODOLambdaJava.controller;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import dev.filipe.TODOLambdaJava.Controller.CreateTaskHandler;
+import dev.filipe.TODOLambdaJava.Controller.ListTasksHandler;
+import dev.filipe.TODOLambdaJava.Model.Task;
+import dev.filipe.TODOLambdaJava.repository.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ListTaskHandlerTest {
+
+    @Mock
+    private Context context;
+
+    @Mock
+    private LambdaLogger logger;
+
+    @Mock
+    private DynamoDbTable<Task> taskTable;
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    private ListTasksHandler listTasksHandler ;
+
+    private final Gson gson = new Gson();
+
+    @BeforeEach
+    void setUp() {
+        listTasksHandler = new ListTasksHandler(taskRepository);
+
+
+        when(context.getLogger()).thenReturn(logger);
+    }
+
+    @Test
+    void testHandleResponse() {
+
+        String userId = "user-id-123";
+
+        Task task = new Task();
+        task.setTitle("Task 1");
+
+        List<Task> listTasks = List.of(task);
+
+        when(taskRepository.listTasks(userId)).thenReturn(listTasks);
+
+        APIGatewayProxyRequestEvent request= new APIGatewayProxyRequestEvent();
+
+        APIGatewayProxyResponseEvent response = listTasksHandler.handleRequest(request, context);
+        assertEquals(200, response.getStatusCode());
+        assertEquals(gson.toJson(listTasks), response.getBody());
+
+
+        verify(taskRepository).listTasks(userId);
+
+    }
+
+}
