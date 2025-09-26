3728092e0167767501457ff8813b0c8468e346b7
slimafilipe <limafilipe.coding@gmail.com>
2025-09-26 13:21:49 -0300

    feat: add method listTasks

--- src/main/java/dev/filipe/TODOLambdaJava/repository/TaskRepository.java
+++ src/main/java/dev/filipe/TODOLambdaJava/repository/TaskRepository.java
@@ -1,4 +1,22 @@
 package dev.filipe.TODOLambdaJava.repository;
 
+import dev.filipe.TODOLambdaJava.Model.Task;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+
+import java.util.List;
+
 public class TaskRepository {
+    private static DynamoDbTable<Task> taskTable;;
+    // Construtor para injeção de dependência em testes
+    public TaskRepository(DynamoDbTable<Task> taskTable) {
+        TaskRepository.taskTable = taskTable;
+    }
+
+    public static List<Task> listTasks(String userId) {
+        QueryConditional conditional =  QueryConditional.keyEqualTo(Key.builder().partitionValue(userId).build());
+
+        return taskTable.query(conditional).items().stream().toList();
+    }
 }
