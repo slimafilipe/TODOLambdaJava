a417cbfb3acd551d1d3a0de0be64bb2ea28061bc
slimafilipe <limafilipe.coding@gmail.com>
2025-10-12 17:30:40 -0300

    feat: Add constants variable

---
+++ src/main/java/dev/filipe/TODOLambdaJava/model/constants/Constants.java
@@ -0,0 +1,8 @@
+package dev.filipe.TODOLambdaJava.model.constants;
+
+public final class Constants {
+    private Constants(){}
+
+    public static final String USER_PREFIX = "USER#";
+    public static final String TASK_PREFIX = "TASK#";
+}
