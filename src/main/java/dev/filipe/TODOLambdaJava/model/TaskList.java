be18bbfa436c17b605fd528644149f6c3924b4c6
slimafilipe <limafilipe.coding@gmail.com>
2025-10-23 02:28:52 -0300

    feat: Add model TaskList

---
+++ src/main/java/dev/filipe/TODOLambdaJava/model/TaskList.java
@@ -0,0 +1,47 @@
+package dev.filipe.TODOLambdaJava.model;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+@DynamoDbBean
+public class TaskList {
+    private String userId;
+    private String taskListId;
+    private String listName;
+
+    public TaskList() {
+    }
+
+    public TaskList(String userId, String taskListId, String listName) {
+        this.userId = userId;
+        this.taskListId = taskListId;
+        this.listName = listName;
+    }
+
+    @DynamoDbPartitionKey
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDbSortKey
+    public String getTaskListId() {
+        return taskListId;
+    }
+
+    public void setTaskListId(String taskListId) {
+        this.taskListId = taskListId;
+    }
+
+    public String getListName() {
+        return listName;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+}
