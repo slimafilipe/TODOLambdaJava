ddd6bfeb8a69d162b5efe59371f7b7220abfae63
slimafilipe <limafilipe.coding@gmail.com>
2025-10-25 02:00:02 -0300

    feat: Add method getUserEmail to extract email in claims of cognito

--- src/main/java/dev/filipe/TODOLambdaJava/util/AuthUtils.java
+++ src/main/java/dev/filipe/TODOLambdaJava/util/AuthUtils.java
@@ -1,11 +1,15 @@
 package dev.filipe.TODOLambdaJava.util;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Optional;
 
 public final class AuthUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthUtils.class);
 
     private AuthUtils(){}
 
@@ -25,6 +29,26 @@ public final class AuthUtils {
                 }
             }
         } catch (Exception e){
+            return Optional.empty();
+        }
+        return Optional.empty();
+    }
+    public static Optional<String> getUserEmail(APIGatewayProxyRequestEvent input) {
+        try {
+            var requestContext = input.getRequestContext();
+            var authorizer = requestContext.getAuthorizer();
+
+            Object claimsObject = authorizer.get("claims");
+
+            if (claimsObject instanceof Map) {
+                @SuppressWarnings("unchecked")
+                Map<String, String> claims = (Map<String, String>) authorizer.get("claims");
+                String userEmail = claims.get("email");
+                if (userEmail != null && !userEmail.isEmpty()) {
+                    return Optional.of(userEmail);
+                }
+            }
+        }catch (Exception e){
             return Optional.empty();
         }
         return Optional.empty();
