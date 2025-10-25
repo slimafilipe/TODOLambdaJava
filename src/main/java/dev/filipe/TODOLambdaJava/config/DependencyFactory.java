f37e421c311643554fc09fce44c24a558f1354e3
slimafilipe <limafilipe.coding@gmail.com>
2025-10-25 02:01:13 -0300

    feat: Add dependecy injection SqsClient

--- src/main/java/dev/filipe/TODOLambdaJava/config/DependencyFactory.java
+++ src/main/java/dev/filipe/TODOLambdaJava/config/DependencyFactory.java
@@ -8,18 +8,20 @@ import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
 public class DependencyFactory {
     private static final TaskRepository taskRepositoryInstance;
     private static final TaskListRepository taskListRepository;
+    private static final SqsClient sqsClientInstance;
 
     static {
         DynamoDbClient dynamoDbClient = DynamoDbClient.builder().build();
         DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient).build();
+        sqsClientInstance = SqsClient.builder().build();
         String TABLE_NAME = System.getenv("TASKS_TABLE");
         DynamoDbTable<Task> taskTable = enhancedClient.table(TABLE_NAME, TableSchema.fromBean(Task.class));
         DynamoDbTable<TaskList> taskListTable = enhancedClient.table(TABLE_NAME, TableSchema.fromBean(TaskList.class));
-
         taskRepositoryInstance = new TaskRepository(taskTable);
         taskListRepository = new TaskListRepository(taskListTable);
 
@@ -30,6 +32,9 @@ public class DependencyFactory {
     public static TaskListRepository getTaskListRepository(){ return taskListRepository; }
     public static TaskRepository getTaskRepository(){
         return taskRepositoryInstance;
+    }
+    public static SqsClient getSqsClient(){
+        return sqsClientInstance;
     }
 
 }
